1d1d0736a1a5a5e9732949ba394aa0cf764f751d
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2021-04-02 23:28:45 +0600

    april 2, 2021

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 import logo from "./logo.png";
-import InvoivePackage from './Component ';
+import InvoivePackage from "./Component ";
 
-window.addEventListener("load", (event) => { alert("Please open with PC or Laptop or Big screens or rotate or phone for better experience")});
+window.addEventListener("load", (event) => {
+  alert(
+    "Please open with PC or Laptop or Big screens or rotate or phone for better experience"
+  );
+});
 
 function Alert({ data }) {
   return (
-    <div className="alert" >
+    <div className="alert">
       <p>{data}</p>
     </div>
   );
 }
 
 function App() {
-
   var DATE = new Date();
-  var toDAY = DATE.getDate()+'-'+(DATE.getMonth()+1)+'-'+DATE.getFullYear();
+  var toDAY =
+    DATE.getDate() + "-" + (DATE.getMonth() + 1) + "-" + DATE.getFullYear();
 
-  const addComponent = function() {
+  const addComponent = function () {
     var span = document.createElement("span");
     span.innerText = toDAY;
     var datee = document.createElement("p");
@@ -36,17 +40,20 @@ function App() {
     hder.appendChild(datee);
     hder.appendChild(logoi);
     hder.appendChild(title);
-    
+
     var nav = document.createElement("nav");
-    nav.innerHTML = "<p>Shop # 15 and #21 , Doreen Shopping Center, Plot # 04, Rupnagar Abashik Main Road, Mirpur Dhaka,Bangladesh <br />Facebook/ShuvroJaya Call:01716-126231</p>";
+    nav.innerHTML =
+      "<p>Shop # 15 and #21 , Doreen Shopping Center, Plot # 04, Rupnagar Abashik Main Road, Mirpur Dhaka,Bangladesh <br />Facebook/ShuvroJaya Call:01716-126231</p>";
 
     var threediv = document.createElement("div");
     threediv.classList.add("name");
-    threediv.innerHTML = '<div><strong>Name:</strong>&nbsp;<input type="text" required></input></div><div><strong>Phone No:</strong>&nbsp;<input className="telephon" type="tel" defaultValue="+88" required></input></div><div><strong>Bill:</strong>&nbsp;<input type="number" required></input></div><div><strong>Address:</strong>&nbsp;<textarea type="text" required></textarea></div><div><strong>Transporter:</strong>&nbsp;<input type="text"></input></div><div><strong>Quantity:</strong>&nbsp;<input type="number"></input></div><div><strong>Details:</strong>&nbsp;<textarea type="text" required></textarea></div>';
+    threediv.innerHTML =
+      '<div><strong>Name:</strong>&nbsp;<input type="text" required></input></div><div><strong>Phone No:</strong>&nbsp;<input className="telephon" type="tel" defaultValue="+88" required></input></div><div><strong>Bill:</strong>&nbsp;<input type="number" required></input></div><div><strong>Address:</strong>&nbsp;<textarea type="text" required></textarea></div><div><strong>Transporter:</strong>&nbsp;<input type="text"></input></div><div><strong>Quantity:</strong>&nbsp;<input type="number"></input></div><div><strong>Details:</strong>&nbsp;<textarea type="text" required></textarea></div>';
 
     var cpyrt = document.createElement("div");
     cpyrt.classList.add("cpyright");
-    cpyrt.innerHTML = "<p className='cprt'>Created and updated by <a href='http://akhlak-hossain-jim.github.io/' target='_blank' rel='noopener noreferrer'>&copy;&nbsp;<strong><em>Akhlak Hossain Jim</em></strong></a>.</p>";
+    cpyrt.innerHTML =
+      "<p className='cprt'>Created and updated by <a href='http://akhlak-hossain-jim.github.io/' target='_blank' rel='noopener noreferrer'>&copy;&nbsp;<strong><em>Akhlak Hossain Jim</em></strong></a>.</p>";
 
     var driv = document.createElement("div");
     driv.classList.add("no1");
@@ -72,22 +79,24 @@ function App() {
     var hder2 = document.createElement("header");
     hder2.appendChild(logoii);
     hder2.appendChild(title2);
-    
+
     var nav2 = document.createElement("nav");
-    nav2.innerHTML = "<p>Shop # 15 and #21 , Doreen Shopping Center, Plot # 04, Rupnagar Abashik Main Road, Mirpur Dhaka,Bangladesh <br />Facebook/ShuvroJaya Call:01716-126231</p>";
+    nav2.innerHTML =
+      "<p>Shop # 15 and #21 , Doreen Shopping Center, Plot # 04, Rupnagar Abashik Main Road, Mirpur Dhaka,Bangladesh <br />Facebook/ShuvroJaya Call:01716-126231</p>";
 
     var threediv2 = document.createElement("div");
     threediv2.classList.add("name");
-    threediv2.innerHTML = '<div><strong>Name:</strong>&nbsp;<input type="text" required></input></div><div><strong>Phone No:</strong>&nbsp;<input className="telephon" type="tel" defaultValue="+88" required></input></div><div><strong>Bill:</strong>&nbsp;<input type="number" required></input></div><div><strong>Address:</strong>&nbsp;<textarea type="text" required></textarea></div><div><strong>Transporter:</strong>&nbsp;<input type="text"></input></div><div><strong>Quantity:</strong>&nbsp;<input type="number"></input></div><div><strong>Details:</strong>&nbsp;<textarea type="text" required></textarea></div>';
-
+    threediv2.innerHTML =
+      '<div><strong>Name:</strong>&nbsp;<input type="text" required></input></div><div><strong>Phone No:</strong>&nbsp;<input className="telephon" type="tel" defaultValue="+88" required></input></div><div><strong>Bill:</strong>&nbsp;<input type="number" required></input></div><div><strong>Address:</strong>&nbsp;<textarea type="text" required></textarea></div><div><strong>Transporter:</strong>&nbsp;<input type="text"></input></div><div><strong>Quantity:</strong>&nbsp;<input type="number"></input></div><div><strong>Details:</strong>&nbsp;<textarea type="text" required></textarea></div>';
 
     var cpyrt2 = document.createElement("div");
     cpyrt2.classList.add("cpyright");
-    cpyrt2.innerHTML = "<p className='cprt'>Created and updated by <a href='http://akhlak-hossain-jim.github.io/' target='_blank' rel='noopener noreferrer'>&copy;&nbsp;<strong><em>Akhlak Hossain Jim</em></strong></a>.</p>";
+    cpyrt2.innerHTML =
+      "<p className='cprt'>Created and updated by <a href='http://akhlak-hossain-jim.github.io/' target='_blank' rel='noopener noreferrer'>&copy;&nbsp;<strong><em>Akhlak Hossain Jim</em></strong></a>.</p>";
 
     var div2 = document.createElement("div");
     div2.classList.add("no2");
-    div2.appendChild(datee2)
+    div2.appendChild(datee2);
     div2.appendChild(hder2);
     div2.appendChild(nav2);
     div2.appendChild(threediv2);
@@ -98,21 +107,53 @@ function App() {
     newEle.appendChild(driv);
     newEle.appendChild(div2);
 
-
     var mainV = document.querySelector("#invoiceBox");
     mainV.appendChild(newEle);
-  }
+  };
 
   return (
     <div className="App">
       <header className="insheader">
-        <h1>This is a &#160;<span>Simple</span>&#160;<span>Online</span> Invoicing web App.</h1>
+        <h1>
+          This is a &#160;<span>Simple</span>&#160;<span>Online</span> Invoicing
+          web App.
+        </h1>
         <br />
-        <p className="cprt">Created and updated by <a href="http://akhlak-hossain-jim.github.io/" target="_blank" rel="noopener noreferrer">&copy;&nbsp;<strong><em>Akhlak Hossain Jim</em></strong></a>.</p>
+        <p className="cprt">
+          Created and updated by{" "}
+          <a
+            href="http://akhlak-hossain-jim.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            &copy;&nbsp;
+            <strong>
+              <em>Akhlak Hossain Jim</em>
+            </strong>
+          </a>
+          .
+        </p>
         <br />
-        <strong className="cprt sticky">View: <a href="https://github.com/Akhlak-Hossain-Jim/invoice/blob/main/README.md" target="_blank" rel="noopener noreferrer">User Guid</a></strong>
+        <strong className="cprt sticky">
+          View:{" "}
+          <a
+            href="https://github.com/Akhlak-Hossain-Jim/invoice/blob/main/README.md"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            User Guid
+          </a>
+        </strong>
         <br />
-        <strong className="cprt"><a href="https://github.com/Akhlak-Hossain-Jim/invoice/issues" target="_blank" rel="noopener noreferrer">Report a problem.</a></strong>
+        <strong className="cprt">
+          <a
+            href="https://github.com/Akhlak-Hossain-Jim/invoice/issues"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Report a problem.
+          </a>
+        </strong>
         <br />
         <br />
         <Alert data="Never refresh the page before Finishing the task (If you do you may need to type all again." />
@@ -121,7 +162,9 @@ function App() {
         <InvoivePackage />
       </main>
       <div className="button">
-        <button className="btn-grad" onClick={addComponent}>Add</button>
+        <button className="btn-grad" onClick={addComponent}>
+          Add
+        </button>
       </div>
     </div>
   );
